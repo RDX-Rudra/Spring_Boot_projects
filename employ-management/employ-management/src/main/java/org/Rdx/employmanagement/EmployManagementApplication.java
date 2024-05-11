81bb9096b1bce73955e67ca7901ef68872121aab
RDX-Rudra <dasrudra234@gmail.com>
2024-05-11 09:50:05 +0530

    pr em

--- employ-management/employ-management/src/main/java/org/Rdx/employmanagement/EmployManagementApplication.java
+++ employ-management/employ-management/src/main/java/org/Rdx/employmanagement/EmployManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployManagementApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EmployManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployManagementApplication.class, args);
+    }
 }
