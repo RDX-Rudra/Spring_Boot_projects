51e568232d828ca25bef034688ab76316b37d94b
RDX-Rudra <dasrudra234@gmail.com>
2024-05-11 08:06:01 +0530

    project

--- employ-management/employ-management/src/main/java/org/Rdx/employmanagement/EmployEntity.java
+++ employ-management/employ-management/src/main/java/org/Rdx/employmanagement/EmployEntity.java
@@ -1,12 +1,21 @@
 package org.Rdx.employmanagement;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name="EmployEntity")
 public class EmployEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
     private String phNo;
     private String email;
