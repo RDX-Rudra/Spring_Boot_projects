eb35b23dce26a5664e5f23131a2b125a5fca5d3a
RDX-Rudra <dasrudra234@gmail.com>
2024-05-11 08:34:13 +0530

    pr em

--- employ-management/employ-management/src/main/java/org/Rdx/employmanagement/EmployeeServiceImpl.java
+++ employ-management/employ-management/src/main/java/org/Rdx/employmanagement/EmployeeServiceImpl.java
@@ -3,24 +3,41 @@ package org.Rdx.employmanagement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EmployeeServiceImpl implements EmployeeService {
 
-    List<Employee> employees = new ArrayList<>();
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    // List<Employee> employees = new ArrayList<>();
 
     @Override
     public String createEmployee(Employee employee) {
-        employees.add(employee);
+        EmployEntity employEntity = new EmployEntity();
+        BeanUtils.copyProperties(employee, employEntity);
+        employeeRepository.save(employEntity);
         return "saved successfully";
     }
 
     @Override
     public List<Employee> readEmployees() {
+        List<EmployEntity> employeesList = employeeRepository.findAll();
+        List<Employee> employees = new ArrayList<>();
+        for (EmployEntity employEntity : employeesList) {
+            Employee emp = new Employee();
+            emp.setName(employEntity.getName());
+            emp.setId(employEntity.getId());
+            emp.setPhNo(employEntity.getPhNo());
+            emp.setEmail(employEntity.getEmail());
+            employees.add(emp);
+        }
         return employees;
     }
 
     @Override
     public boolean deleteEmployee(Long id) {
-        employees.remove(id);
+        employeeRepository.deleteById(id);
         return true;
     }
 
